ad26c1387d5c26a0394a93ac00e1f713cd2a58b8
Petr Maev <petr.maev94@gmail.com>
2025-04-11 00:41:24 +0300

    add user interactive

--- src/user_interactive.py
+++ src/user_interactive.py
@@ -1,79 +1,86 @@
+from config import config
+from src.db_creater import DBCreater
 from src.db_manager import DBManager
 
+from config import PATH_TO_HH_DATABASE
 
-def get_companies_and_vacancies_count() -> str:
-    """Функция вывода количества вакансий у каждого работодателя из базы данных."""
 
+def get_data_for_create_db():
+    print("Введите данные для подключения к базе данных.\n")
+    your_host = input("Введите хост: ")
+    your_port = input("Введите номер порта: ")
+    database_name = input("Введите имя базы данных: ")
+    user_name = input("Введите имя пользователя: ")
+    your_password = input("Введите пароль: ")
+
+    new_params = f'[postgresql]\nhost={your_host}\nport={your_port}\nuser={user_name}\npassword={your_password}'
+
+    with open(PATH_TO_HH_DATABASE, 'w', encoding='utf-8') as f:
+        f.write(new_params)
+
+    params = config()
+    DBCreater().db_create(params, database_name)
+
+    # Вывод информации по количеству вакансий у работодателей
     while True:
         question_1 = input("Вывести информацию по количеству вакансий у работодателей из базы данных: Да\Нет ").lower()
 
         if question_1 == "да":
             print("Вывожу список работодателей с количеством вакансий:")
-            get_count_db = DBManager().get_companies_and_vacancies_count()
-            return get_count_db
+            get_count_db = DBManager().get_companies_and_vacancies_count(params, database_name)
+            print(get_count_db)
+            break
         elif question_1 == "нет":
             break
         else:
             print(f"Ошибка. Повторите попытку.\n")
 
-
-def get_all_vacancies() -> str:
-    """Функция вывода всех вакансий базы данных."""
-
+    # Вывод информации по всем вакансиям базы данных
     while True:
         question_2 = input("Вывести информацию по всем вакансиям базы данных: Да\Нет ").lower()
 
         if question_2 == "да":
             print("Вывожу список всех вакансий:")
-            get_all_vac = DBManager().get_all_vacancies()
-            return get_all_vac
+            get_all_vac = DBManager().get_all_vacancies(params, database_name)
+            print(get_all_vac)
+            break
         elif question_2 == "нет":
             break
         else:
             print(f"Ошибка. Повторите попытку.\n")
 
-
-def get_avg_salary() -> str:
-    """Функция вывода средней зарплаты вакансий."""
-
+    # Вывода средней зарплаты вакансий.
     while True:
         question_3 = input("Вывести среднюю зарплату вакансий базы данных: Да\Нет ").lower()
 
         if question_3 == "да":
             print("Вывожу среднюю зарплату:")
-            get_avg_sal = DBManager().get_avg_salary()
-            return get_avg_sal
+            get_avg_sal = DBManager().get_avg_salary(params, database_name)
+            print(get_avg_sal)
+            break
         elif question_3 == "нет":
             break
         else:
             print(f"Ошибка. Повторите попытку.\n")
 
-
-def get_vacancies_with_higher_salary() -> str:
-    """Функция вывода вакансий с зарплатой выше средней."""
-
+    # Вывод вакансий с зарплатой выше средней.
     while True:
         question_4 = input("Вывести информацию по вакансиям с зарплатой выше средней: Да\Нет ").lower()
 
         if question_4 == "да":
-            get_high_salary = DBManager().get_vacancies_with_higher_salary()
-            return get_high_salary
+            get_high_salary = DBManager().get_vacancies_with_higher_salary(params, database_name)
+            print(get_high_salary)
+            break
         elif question_4 == "нет":
             break
         else:
             print(f"Ошибка. Повторите попытку.\n")
 
-
-def get_vacancies_with_keyword() -> str:
-    """Функция вывода вакансий по заданному слову."""
+    # Вывод вакансий по заданному слову.
     search_query = input("Введите ключевое слово для фильтрации вакансий: ")
-    get_vacancies_of_keyword = DBManager().get_vacancies_with_keyword(search_query)
-    return get_vacancies_of_keyword
+    get_vacancies_of_keyword = DBManager().get_vacancies_with_keyword(search_query, params, database_name)
+    print(get_vacancies_of_keyword)
 
 
 if __name__ == "__main__":
-    # get_companies_and_vacancies_count()
-    # get_all_vacancies()
-    # get_avg_salary()
-    # get_vacancies_with_higher_salary()
-    get_vacancies_with_keyword()
+    get_data_for_create_db()
